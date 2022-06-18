5220ef352997194d91d3f6614bacccdd9949cced
daur-vip <mail@daur.vip>
2022-06-18 22:15:46 +0600

    some changes in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,6 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
                     {{ __('Register') }}
                 </a>
